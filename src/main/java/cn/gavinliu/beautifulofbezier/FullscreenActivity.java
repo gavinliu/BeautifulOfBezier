6b001a635eeeb3781711ad8a60e1370e681dcedb
刘云龙 <gavin6liu@gmail.com>
2015-03-31 16:10:38 +0800

    first commit

---
+++ src/main/java/cn/gavinliu/beautifulofbezier/FullscreenActivity.java
@@ -0,0 +1,50 @@
+package cn.gavinliu.beautifulofbezier;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import cn.gavinliu.beautifulofbezier.util.SystemUiHider;
+
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ *
+ * @see SystemUiHider
+ */
+public class FullscreenActivity extends Activity {
+
+    private static final boolean TOGGLE_ON_CLICK = true;
+
+    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
+
+    private SystemUiHider mSystemUiHider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen);
+
+        final View contentView = findViewById(R.id.fullscreen_content);
+
+        mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
+        mSystemUiHider.setup();
+        mSystemUiHider.hide();
+
+        contentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (TOGGLE_ON_CLICK) {
+                    mSystemUiHider.toggle();
+                } else {
+                    mSystemUiHider.show();
+                }
+            }
+        });
+
+        if (getActionBar() != null)
+            getActionBar().hide();
+    }
+
+}
