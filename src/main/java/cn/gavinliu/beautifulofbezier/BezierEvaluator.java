ae96502d8fcb21504d5d7ebe61be76ff9b0c972c
刘云龙 <gavin6liu@gmail.com>
2015-04-02 16:03:29 +0800

    Update BezierEvaluator

--- src/main/java/cn/gavinliu/beautifulofbezier/BezierEvaluator.java
+++ src/main/java/cn/gavinliu/beautifulofbezier/BezierEvaluator.java
@@ -1,15 +1,40 @@
 package cn.gavinliu.beautifulofbezier;
 
 import android.animation.TypeEvaluator;
+import android.graphics.PointF;
 
 /**
  * Created by gavin on 15-4-2.
  */
-public class BezierEvaluator implements TypeEvaluator {
+public class BezierEvaluator implements TypeEvaluator<PointF> {
+
+    private PointF points[];
+
+    public BezierEvaluator(PointF... points) {
+        if (points.length != 4) {
+            throw new IllegalArgumentException("只演示三次方贝赛尔曲线");
+        }
+        this.points = points;
+    }
 
     @Override
-    public Object evaluate(float fraction, Object startValue, Object endValue) {
-        return null;
+    public PointF evaluate(float fraction, PointF startValue, PointF endValue) {
+        // B(t) = P0 * (1-t)^3 + 3 * P1 * t * (1-t)^2 + 3 * P2 * t^2 * (1-t) + P3 * t^3
+
+        float t = fraction;
+        float one_t = 1.0f - t;
+
+        PointF P0 = points[0];
+        PointF P1 = points[1];
+        PointF P2 = points[2];
+        PointF P3 = points[3];
+
+        float x = (float) (P0.x * Math.pow(one_t, 3) + 3 * P1.x * t * Math.pow(one_t, 2) + 3 * P2.x * Math.pow(t, 2) * one_t + P3.x * Math.pow(t, 3));
+        float y = (float) (P0.y * Math.pow(one_t, 3) + 3 * P1.y * t * Math.pow(one_t, 2) + 3 * P2.y * Math.pow(t, 2) * one_t + P3.y * Math.pow(t, 3));
+
+        PointF pointF = new PointF(x, y);
+
+        return pointF;
     }
 
 }
--- src/main/java/cn/gavinliu/beautifulofbezier/BezierEvaluatorActivity.java
+++ src/main/java/cn/gavinliu/beautifulofbezier/BezierEvaluatorActivity.java
@@ -1,7 +1,11 @@
 package cn.gavinliu.beautifulofbezier;
 
+import android.animation.ValueAnimator;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.animation.AccelerateInterpolator;
+import android.widget.TextView;
 
 /**
  * Created by gavin on 15-4-2.
@@ -12,6 +16,31 @@ public class BezierEvaluatorActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bezier_evaluator);
+
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
+        PointF p0 = new PointF(100, 100);
+        PointF p1 = new PointF(800, 100);
+        PointF p2 = new PointF(100, 800);
+        PointF p3 = new PointF(800, 800);
+
+        BezierEvaluator evaluator = new BezierEvaluator(p0, p1, p2, p3);
+
+        ValueAnimator animator = ValueAnimator.ofObject(evaluator, p0, p3)
+                .setDuration(4500);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                PointF pointF = (PointF) animation.getAnimatedValue();
+                textView.setX(pointF.x);
+                textView.setY(pointF.y);
+            }
+
+        });
+
+        animator.start();
+
     }
 
 }
